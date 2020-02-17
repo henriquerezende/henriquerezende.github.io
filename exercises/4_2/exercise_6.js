9e7366f803140f9aae8cfbffd4acaadb80002f4a
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-17 18:46:28 -0300

    Incrementando exercicio 6

--- exercises/4_2/exercise_6.js
+++ exercises/4_2/exercise_6.js
@@ -1,11 +1,15 @@
-let num = 1
-let ePrimo = true
+let num = 37;
+let ePrimo = true;
 
-if(num==2 || num<1) { ePrimo = false }
-
-for(let cont = 3; cont<num; cont+=2){
-    if(num%cont == 0){
-        ePrimo = false;
-    }    
+if (num == 2 || num < 1 || num % 2 == 0) {
+  ePrimo = false;
+} else {
+  for (let cont = 3; cont < num / 2; cont += 2) {
+    console.log(cont);
+    if (num % cont == 0) {
+      ePrimo = false;
+    }
+  }
 }
-(ePrimo==true) ? console.log("É primo") : console.log("Não é primo")
+
+ePrimo == true ? console.log("É primo") : console.log("Não é primo");
