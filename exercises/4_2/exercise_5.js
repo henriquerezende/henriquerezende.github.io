cde716a88d26b8cf89eb2bb5ff0c2d732c0ac1ef
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-18 15:01:12 -0300

    Melhorando exercicio 5

--- exercises/4_2/exercise_5.js
+++ exercises/4_2/exercise_5.js
@@ -6,20 +6,16 @@ for (let cont = 0; cont < n; cont++) {
   string[cont] = " ";
 }
 
-let meio1 = Math.trunc(n/2)
-let meio2 = meio1
-for (let cont3 = 0; cont3 < n; cont3+=2){
-  string[meio1]="*"
-  string[meio2]="*"  
-  if(cont3>=n-1){
+let meio = Math.trunc(n/2)
+for (let cont3 = 0; cont3 < n/2; cont3++){
+  string[meio + cont3]="*"
+  string[meio - cont3]="*"  
+  if(cont3>=(n/2)-1){
     for(cont2 = 0; cont2<n; cont2++){
         string[cont2]="*"
     }
   }
-  let arrayToString = string.join("");
-  console.log(arrayToString)  
-  string[meio1]=" "
-  string[meio2]=" "
-  meio1++
-  meio2--  
+  console.log(string.join(""))  
+  string[meio + cont3]=" "
+  string[meio - cont3]=" "
 }
