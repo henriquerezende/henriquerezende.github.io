9e7a6c1d247267f8e603b4cf70c69f12408f829d
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-18 17:59:03 -0300

    Exercicio bonus: Algaritmos Romanos para decimal

---
+++ exercises/4_3/bonusRomano.js
@@ -0,0 +1,60 @@
+//(Difícil) Faça um programa que recebe uma string em algarismos romanos e retorna o número que a string representa.
+//E atenção! Esse exercício já apareceu no processo seletivo de uma grande multinacional!
+
+//Dicas:
+
+//A função .split("") transforma uma string em um array de caracteres, em que cada elemento do array é uma letra.
+//O valor de cada numeral romano é:
+
+//Copiar
+//| I   | 1    |
+//| --- | ---- |
+//| IV  | 4    |
+//| V   | 5    |
+//| IX  | 9    |
+//| X   | 10   |
+//| XL  | 40   |
+// | L   | 50   |
+// | XC  | 90   |
+// | C   | 100  |
+// | CD  | 400  |
+// | D   | 500  |
+// | CM  | 900  |
+// | M   | 1000 |
+// Que tal criar um objeto que associa cada letra a um numeral para fácil consulta?
+
+// Atenção! Quando você tem um número pequeno à direita de um número grande, eles devem ser somados. 
+//Exemplo: XI = 10 + 1 = 11. No entanto, se o número pequeno está à esquerda de um número maior que ele,
+// ele deve ser subtraído. Exemplo: IX = 10 - 1 = 9.
+
+// LXXXVIII = 88
+const romToDec = string => {
+    let romToDec = {
+        I : 1,
+        V : 5,
+        X : 10,
+        L : 50,
+        C : 100,
+        D : 500,
+        M : 1000,
+    }
+    return romToDec[string]
+}
+
+const somaRomanos = (string) => {
+    let array = []
+    let soma = 0
+    for(Letra of string){       
+        array.push(romToDec(Letra))        
+    }
+    for(indNum in array){
+        if(array[parseInt(indNum)+1]>array[indNum]){
+            soma -= array[indNum]
+        } else {
+            soma += array[indNum]
+        }
+    }
+    return soma
+}
+
+console.log(somaRomanos("LXXXIV"))
