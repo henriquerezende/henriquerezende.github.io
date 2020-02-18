e99f76491cef970e9bab67912258a5f9029ce0f2
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-18 13:37:28 -0300

    Melhorando exercicio 4

--- exercises/4_2/exercise_4.js
+++ exercises/4_2/exercise_4.js
@@ -1,18 +1,13 @@
 
-n = 5;
+n = 9;
 let string = [];
 
 for (let cont = 0; cont < n; cont++) {
   string[cont] = " ";
 }
 
-let meio1 = Math.round(n/2)
-let meio2 = meio1
-for (let cont3 = 0; cont3 < n; cont3+=2){
-  string[meio1]="*"
-  string[meio2]="*"
-  meio1++
-  meio2--
-  let arrayToString = string.join("");
-  console.log(arrayToString)
+for (let cont3 = 0; cont3 < n/2; cont3++){
+  string[Math.round(n/2) + cont3]="*"
+  string[Math.round(n/2) - cont3]="*"
+  console.log(string.join(""))
 }
