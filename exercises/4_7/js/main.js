3f10c7b62a33a4a009f27a8f988c9d51f1372ff8
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-27 17:25:24 -0300

    Adicionando botoes que mudam tamanho da fonte e espacamento entre linhas

--- exercises/4_7/js/main.js
+++ exercises/4_7/js/main.js
@@ -13,13 +13,37 @@ window.onload = () =>{
         }
     }
 
+    const changeTextSize = () => {
+        document.getElementsByTagName("body")[0].style.fontSize = document.getElementById("fontSize").value + "px"
+        if(typeof(Storage) != "undefined"){
+            localStorage.setItem("fontSize", document.getElementById("fontSize").value + "px")
+        }
+    }
+
+    const changeLineHeight= () => {
+        document.getElementsByTagName("body")[0].style.lineHeight = document.getElementById("lineHeight").value + "px"
+        if(typeof(Storage) != "undefined"){
+            localStorage.setItem("lineHeight", document.getElementById("lineHeight").value + "px")
+        }
+    }
+
+
+
     if(localStorage.getItem("bgColor") && typeof(Storage) != "undefined"){
         document.getElementsByTagName("body")[0].style.backgroundColor = localStorage.getItem("bgColor")
     }
     if(localStorage.getItem("textColor") && typeof(Storage) != "undefined"){
         document.getElementsByTagName("body")[0].style.color = localStorage.getItem("textColor")
     }
+    if(localStorage.getItem("fontSize") && typeof(Storage) != "undefined"){
+        document.getElementsByTagName("body")[0].style.fontSize = localStorage.getItem("fontSize")
+    }
+    if(localStorage.getItem("lineHeight") && typeof(Storage) != "undefined"){
+        document.getElementsByTagName("body")[0].style.lineHeight = localStorage.getItem("lineHeight")
+    }
 
     document.getElementById("backgroundColor").addEventListener("change", changeBackground)
     document.getElementById("textColor").addEventListener("change", changeTextColor)   
+    document.getElementById("fontSize").addEventListener("change", changeTextSize)
+    document.getElementById("lineHeight").addEventListener("change", changeLineHeight)
 }
