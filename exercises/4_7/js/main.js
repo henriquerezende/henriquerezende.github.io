16159cbaa79382e6f0f8ede2e65193a9f0cce212
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-27 17:05:43 -0300

    Adicionando botao que muda cor do texto

--- exercises/4_7/js/main.js
+++ exercises/4_7/js/main.js
@@ -1,12 +1,25 @@
 window.onload = () =>{
-    if(localStorage.getItem("bgColor")){
-        document.getElementsByTagName("body")[0].style.backgroundColor = localStorage.getItem("bgColor")
-    }
     const changeBackground = () => {
         document.getElementsByTagName("body")[0].style.backgroundColor = document.getElementById("backgroundColor").value
         if(typeof(Storage) != "undefined"){
             localStorage.setItem("bgColor", document.getElementById("backgroundColor").value)
         }
     }
-    document.getElementById("backgroundColor").addEventListener("change", changeBackground)    
+
+    const changeTextColor = () => {
+        document.getElementsByTagName("body")[0].style.color = document.getElementById("textColor").value
+        if(typeof(Storage) != "undefined"){
+            localStorage.setItem("textColor", document.getElementById("textColor").value)
+        }
+    }
+
+    if(localStorage.getItem("bgColor") && typeof(Storage) != "undefined"){
+        document.getElementsByTagName("body")[0].style.backgroundColor = localStorage.getItem("bgColor")
+    }
+    if(localStorage.getItem("textColor") && typeof(Storage) != "undefined"){
+        document.getElementsByTagName("body")[0].style.color = localStorage.getItem("textColor")
+    }
+
+    document.getElementById("backgroundColor").addEventListener("change", changeBackground)
+    document.getElementById("textColor").addEventListener("change", changeTextColor)   
 }
