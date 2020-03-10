4974f214fccc50807a07adc734adf7b4d86c8048
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-10 16:45:30 -0300

    atualizando projeto mistery letter

--- projects/mistery-letter-project/script.js
+++ projects/mistery-letter-project/script.js
@@ -1,18 +1,18 @@
 window.onload = () => {
-  const classes = [["newspaper", "magazine1", "magazine2"], ['medium', 'big', 'reallybig'], ['rotateleft', 'rotateright'], ['skewleft', 'skewright']]
+  const classes = [['newspaper', 'magazine1', 'magazine2'], ['medium', 'big', 'reallybig'], ['rotateleft', 'rotateright'], ['skewleft', 'skewright']];
 
-  document.getElementById("criar-carta").addEventListener("click", () => {
+  document.getElementById('criar-carta').addEventListener('click', () => {
     const randomNumber = (max) => Math.trunc(max * Math.random());
     const randomClass = () => `${classes[0][randomNumber(3)]} ${classes[1][randomNumber(3)]} ${classes[2][randomNumber(2)]} ${classes[3][randomNumber(2)]}`;
-    document.getElementById("carta-gerada").innerHTML = ''
-    const palavras = document.getElementById("carta-texto").value.split(' ');
-    document.getElementById("carta-contador").textContent = palavras.length;
-    palavras.forEach(element => {
-      const span = document.createElement("span");
+    document.getElementById('carta-gerada').innerHTML = '';
+    const palavras = document.getElementById('carta-texto').value.split(' ');
+    document.getElementById('carta-contador').textContent = palavras.length;
+    palavras.forEach((element) => {
+      const span = document.createElement('span');
       span.textContent = element;
       span.classList = randomClass();
-      span.addEventListener('click', (event) => event.target.classList = randomClass());
-      document.getElementById("carta-gerada").appendChild(span);
+      span.addEventListener('click', (event) => { event.target.classList = randomClass(); });
+      document.getElementById('carta-gerada').appendChild(span);
     });
   });
 };
